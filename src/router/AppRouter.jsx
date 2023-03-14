29f2b54236f9f10bace7617a838e9ec49137f50c
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-03-14 21:31:42 +0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AppRouter = () => {
+  return <div>AppRouter</div>;
+};
+
+export default AppRouter;
+// apiKey: "AIzaSyDTmfx82lSzsB19wWcoxGB4221awGD-cZc",
+// authDomain: "movie-app-in.firebaseapp.com",
+// projectId: "movie-app-in",
+// storageBucket: "movie-app-in.appspot.com",
+// messagingSenderId: "804338685930",
+// appId: "1:804338685930:web:ea74084331339a44c44493"
